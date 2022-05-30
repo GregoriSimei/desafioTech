876964f811b50d0b378655d018181e5f66850981
GregoriSimei <gregorisimei@gmail.com>
2022-05-30 05:02:33 -0300

    Exam repository methods implemented

---
+++ src/modules/exam/Repositories/ExamRepository.ts
@@ -0,0 +1,22 @@
+import { Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ExamDTO } from '../DTO/ExamDTO';
+import { IExamRepository } from './IExamRepository';
+
+export class ExamRepository implements IExamRepository {
+  constructor(
+    @Inject('TYPEORM_ENTITY_EXAM')
+    private examRepository: Repository<ExamDTO>,
+  ) {}
+
+  async findAll(): Promise<ExamDTO[]> {
+    return this.examRepository.find({});
+  }
+  async update(exam: ExamDTO): Promise<ExamDTO> {
+    await this.examRepository.update(exam.id, exam);
+    return this.examRepository.findOneBy({ id: exam.id });
+  }
+  async remove(examId: number): Promise<void> {
+    await this.examRepository.delete(examId);
+  }
+}
